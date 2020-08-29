fac21193b0d8775f1d38d9a595153982926549f8
Shashank Sharma <shashank.ns67@gmail.com>
2020-08-29 19:51:20 +0530

    User can delete profile now - added delete route for profile - it also deletes the user - profile routes are done

--- devServer/routes/api/profile.js
+++ devServer/routes/api/profile.js
@@ -284,4 +284,19 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
         .catch(err => res.status(404).json(err));
 });
 
+/**
+ * @route               DELETE api/profile
+ * @description         delete user and profile
+ * @access              private
+ */
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+        .then(() => {
+            User.findOneAndRemove({ _id: req.user.id })
+                .then(() => res.json({ success: true }))
+                .catch(err => res.status(404).json(err));
+        })
+        .catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
