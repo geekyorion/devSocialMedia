7171870c45d4c6d8cce6413bac57a01c4bcefd27
Shashank Sharma <shashank.ns67@gmail.com>
2020-09-26 17:13:34 +0530

    converted to functional component

--- devUI/src/components/auth/Login.js
+++ devUI/src/components/auth/Login.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class Login extends Component {
     constructor() {
         super();
-
     }
     render() {
         return (
--- devUI/src/components/auth/Register.js
+++ devUI/src/components/auth/Register.js
@@ -1,98 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Register extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            cpassword: '',
-            errors: {},
-        };
-    }
+const Register = () => {
+    const [fullname, setFullname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cpassword, setCpassword] = useState('');
+    const [errors, setErrors] = useState({});
 
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-
-    registerUser = (e) => {
+    const registerUser = (e) => {
         e.preventDefault();
 
         const newUser = {
-            name: this.state.name,
-            email: this.state.email,
-            password: this.state.password,
-            cPassword: this.state.cpassword
+            name: fullname,
+            email,
+            password,
+            cPassword: cpassword
         };
 
         console.log(newUser);
         return false;
     }
 
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-8 m-auto">
-                    <h1 className="display-4 text-center">Sign Up</h1>
-                    <p className="lead text-center">
-                        Create your Dev Social Media account
-                    </p>
-                    <form action="create-profile.html" onSubmit={this.registerUser}>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control form-control-lg"
-                                placeholder="Name"
-                                name="name"
-                                value={this.state.name}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="email"
-                                className="form-control form-control-lg"
-                                placeholder="Email Address"
-                                name="email"
-                                value={this.state.email}
-                                onChange={this.handleChange}
-                            />
-
-                            <small className="form-text text-muted">
-                                This site uses Gravatar so if you want a profile
-                                image, use a Gravatar email
-                            </small>
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control form-control-lg"
-                                placeholder="Password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control form-control-lg"
-                                placeholder="Confirm Password"
-                                name="cpassword"
-                                value={this.state.cpassword}
-                                onChange={this.handleChange}
-                            />
-                        </div>
+    return (
+        <div className="row">
+            <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Sign Up</h1>
+                <p className="lead text-center">
+                    Create your Dev Social Media account
+                </p>
+                <form action="create-profile.html" onSubmit={registerUser}>
+                    <div className="form-group">
                         <input
-                            type="submit"
-                            className="btn btn-info btn-block mt-4"
+                            type="text"
+                            className="form-control form-control-lg"
+                            placeholder="Name"
+                            name="name"
+                            value={fullname}
+                            onChange={(e) => setFullname(e.target.value)}
                         />
-                    </form>
-                </div>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-control form-control-lg"
+                            placeholder="Email Address"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                        <small className="form-text text-muted">
+                            This site uses Gravatar so if you want a profile
+                            image, use a Gravatar email
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control form-control-lg"
+                            placeholder="Password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control form-control-lg"
+                            placeholder="Confirm Password"
+                            name="cpassword"
+                            value={cpassword}
+                            onChange={(e) => setCpassword(e.target.value)}
+                        />
+                    </div>
+                    <input
+                        type="submit"
+                        className="btn btn-info btn-block mt-4"
+                    />
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Register;
