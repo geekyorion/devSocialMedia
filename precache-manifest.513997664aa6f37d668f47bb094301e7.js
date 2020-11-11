50f1e2346b6b73ffc80bd3b69b3ecf675865d0a5
Shashank Sharma <shashank.ns67@gmail.com>
2020-11-11 21:37:24 +0530

    Updates

---
+++ precache-manifest.513997664aa6f37d668f47bb094301e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1762a313a0bbfc6fde0f92a18000400",
+    "url": "/devSocialMedia/index.html"
+  },
+  {
+    "revision": "ea60085359313bfd2ec6",
+    "url": "/devSocialMedia/static/css/2.7b9c825f.chunk.css"
+  },
+  {
+    "revision": "5241abef861e5061b69e",
+    "url": "/devSocialMedia/static/css/main.696abd36.chunk.css"
+  },
+  {
+    "revision": "ea60085359313bfd2ec6",
+    "url": "/devSocialMedia/static/js/2.f238fee4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/devSocialMedia/static/js/2.f238fee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5241abef861e5061b69e",
+    "url": "/devSocialMedia/static/js/main.29204b46.chunk.js"
+  },
+  {
+    "revision": "9736c3264d0e58c2d6ce",
+    "url": "/devSocialMedia/static/js/runtime-main.e9eacde3.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/devSocialMedia/static/media/showcase.4b31330b.jpg"
+  }
+]);
