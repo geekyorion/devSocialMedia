86a20d913a90c0f0d16c5ffd742cefe0aa55581b
Shashank <shashank.ns67@gmail.com>
2023-01-15 23:24:22 +0530

    changed base URL

--- devUI/src/utils/axiosApiEndPoint.js
+++ devUI/src/utils/axiosApiEndPoint.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosAPI = axios.create({
-    baseURL: 'https://dev-social-media-server.herokuapp.com/'
+    // baseURL: 'https://dev-social-media-server.herokuapp.com/'
+    baseURL: 'https://devsocialmedia.glitch.me'
 });
 
 export default axiosAPI;
